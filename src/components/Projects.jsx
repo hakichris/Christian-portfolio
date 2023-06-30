15b9f263c8b3db03fb6a9786cb7de76fe9b79b15
Chris <hakizachrist1@gmail.com>
2023-06-30 17:36:18 +0200

    Update project.js

---
+++ src/components/Projects.jsx
@@ -0,0 +1,13 @@
+
+const ProjectCards =() => {
+
+    return(
+    
+        <div>
+            <h1>welcome to projects</h1>
+      </div>
+    )
+    
+    }
+    
+    export default ProjectCards;
