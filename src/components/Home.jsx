ee5f956638652efd106108e7aa9b11c74d81b2c1
Chris <hakizachrist1@gmail.com>
2023-07-03 20:26:50 +0200

    Update home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,64 @@
-const Profile = () => (
+import '../home-style/mainpage.css';
+import ProjectCards from './Projects';
+import Contact from './Contact';
+import profilepicture from '../images/profilepicture.jpeg';
 
-  <div className="home">
-    <h1>welcome to Home page</h1>
+const Profile = () => (
+  <div className="body">
+    <div className="home">
+      <div className="profiles">
+        <img src={profilepicture} className="profilepicture" alt="myprofile" />
+        <ul className="profile-links">
+          <li>
+            <a href="https://www.linkedin.com/in/hakichris/">Linkedin</a>
+          </li>
+          <li>
+            <a href="https://github.com/hakichris">Github</a>
+          </li>
+          <li>
+            <a href="https://twitter.com/hakichris2">Twitter</a>
+          </li>
+          <li>
+            <a href="https://www.facebook.com/christian.hakizimana.1">Facebook</a>
+          </li>
+        </ul>
+      </div>
+      <div className="intro">
+        <h1 className="head1">
+          Hey there. I’m Christian Hakizimana
+          <span className="green-color">
+            A software engineer with focus on full stack web development
+          </span>
+        </h1>
+        <p className="intro-exp">
+          With a background in developing efficient software applications in a global market.
+          More than
+          {' '}
+          <span className="work-hours bold"> 3 years </span>
+          {' '}
+          of experience, including mentoring
+          {' '}
+          <span className="mentoring bold">10+</span>
+          {' '}
+          junior developers to achieve concrete goals on a strict deadline.
+          Strong skills include
+          {' '}
+          <span className="mentoring bold">
+            {' '}
+            React, Redux, JavaScript, Ruby on Rails, C++, Java, leadership, Interpersonnal skills,
+            Remote pair-programming.
+          </span>
+          {' '}
+        </p>
+      </div>
+    </div>
+    <div>
+      <ProjectCards />
+    </div>
+    {' '}
+    <div>
+      <Contact />
+    </div>
   </div>
 );
 
