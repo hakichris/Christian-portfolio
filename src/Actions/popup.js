46be3243ce439fc100bda5481f9d6b3dd344aa22
Chris <hakizachrist1@gmail.com>
2023-07-05 01:38:31 +0200

    Update cards

--- src/Actions/popup.js
+++ src/Actions/popup.js
@@ -1,8 +1,34 @@
-const Popup = (id) => {
-    
+import { PropTypes } from 'prop-types';
+import '../projectcard-style/card.css';
 
+const Popup = ({ card }) => (
+  <div className="popup-container">
+    <div className="popup-content">
+      <img src={card.image} alt="cardimage" />
+      <h1>{card.title}</h1>
+      <p>{card.explanation}</p>
+      <div className="stacks">
+        <ul>
+          <li>{card.stacks[0]}</li>
+          <li>{card.stacks[1]}</li>
+          <li>{card.stacks[2]}</li>
+        </ul>
+      </div>
+      <div className="btn">
+        <button type="submit">Check source</button>
+        <button type="submit">Live demo</button>
+      </div>
+    </div>
+  </div>
+);
 
-}
-
+Popup.propTypes = {
+  card: PropTypes.shape({
+    image: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    explanation: PropTypes.string.isRequired,
+    stacks: PropTypes.arrayOf(PropTypes.string).isRequired,
+  }).isRequired,
+};
 
 export default Popup;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,56 +1,102 @@
 import { useState } from 'react';
-import popup from '../Actions/popup';
+import Popup from '../Actions/popup';
+import '../projectcard-style/card.css';
 
 const ProjectCards = () => {
+  const [isOpen, setIsOpen] = useState(null);
   const [cards] = useState([
-
     {
       id: '1',
       image: './images/pngwing.com (2).png',
-      title: 'my project',
+      title: 'my project 1',
       stacks: ['stack1', 'stack2', 'stack3'],
       explanation: 'Hello chris you are going to build your projects card',
-      link: 'skdjlkslks',
+      link: ['github link', 'live links'],
 
     },
 
     {
       id: '2',
       image: './images/pngwing.com (2).png',
-      title: 'my project',
+      title: 'my project 2',
       stacks: ['stack1', 'stack2', 'stack3'],
       explanation: 'Hello chris you are going to build your projects card',
-      link: 'skdjlkslks',
+      link: ['github link', 'live links'],
+    },
 
+    {
+      id: '3',
+      image: './images/pngwing.com (2).png',
+      title: 'my project 3',
+      stacks: ['stack1', 'stack2', 'stack3'],
+      explanation: 'Hello chris you are going to build your projects card',
+      link: ['github link', 'live links'],
+    },
+
+    {
+      id: '4',
+      image: './images/pngwing.com (2).png',
+      title: 'my project 4',
+      stacks: ['stack1', 'stack2', 'stack3'],
+      explanation: 'Hello chris you are going to build your projects card',
+      link: ['github link', 'live links'],
+    },
+
+    {
+      id: '5',
+      image: './images/pngwing.com (2).png',
+      title: 'my project 5',
+      stacks: ['stack1', 'stack2', 'stack3'],
+      explanation: 'Hello chris you are going to build your projects card',
+      link: ['github link', 'live links'],
+    },
+
+    {
+      id: '6',
+      image: './images/pngwing.com (2).png',
+      title: 'my project 6',
+      stacks: ['stack1', 'stack2', 'stack3'],
+      explanation: 'Hello chris you are going to build your projects card',
+      link: ['github link', 'live links'],
     },
 
   ]);
 
-  return (
-    <div>
-      <section>
-        <div className="card-container">
-          <h1 className="section-title"> Check my projects</h1>
-          <div className="cards">
-            { cards.map((card) => (
-              <div key={card.id} className="card">
-                <h1>{card.title}</h1>
-                <p>{card.explanation}</p>
-                <div className="stacks">
-                  <ul>
-                    <li>{card.stacks[0]}</li>
-                    <li>{card.stacks[1]}</li>
-                    <li>{card.stacks[2]}</li>
-                  </ul>
-                </div>
-                <button type="submit"> View more details</button>
-              </div>
+  const togglePopup = (id) => {
+    setIsOpen(id);
+  };
 
-            ))}
-          </div>
+  return (
+    <section>
+      <div className="card-container">
+        <h1 className="section-title">Check my projects</h1>
+        <div className="cards">
+          {cards.map((card) => (
+            <div key={card.id} id={card.id} className="card">
+              <img src={card.image} alt="cardimage" />
+              <h1>{card.title}</h1>
+              <div className="stacks">
+                <ul>
+                  <li>{card.stacks[0]}</li>
+                  <li>{card.stacks[1]}</li>
+                  <li>{card.stacks[2]}</li>
+                </ul>
+              </div>
+              <div className="btn">
+                <button type="submit">Check source</button>
+                <button type="submit">Live demo</button>
+                <button type="submit" onClick={() => togglePopup(card.id)}>
+                  View more details
+                </button>
+              </div>
+              {isOpen === card.id && (
+                <Popup isOpen={isOpen === card.id} togglePopup={togglePopup} card={card} />
+              )}
+            </div>
+          ))}
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 };
 
