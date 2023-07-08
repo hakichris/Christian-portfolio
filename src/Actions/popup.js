d9f31f1e9aec5ed28e1939859efc37e25e3c6588
Chris <hakizachrist1@gmail.com>
2023-07-08 14:38:41 +0200

    Update package.json

--- src/Actions/popup.js
+++ src/Actions/popup.js
@@ -4,19 +4,23 @@ import '../projectcard-style/card.css';
 const Popup = ({ card }) => (
   <div className="popup-container">
     <div className="popup-content">
-      <img src={card.image} alt="cardimage" />
-      <h1>{card.title}</h1>
-      <p>{card.explanation}</p>
+      <img className="popupimage" src={card.image} alt="cardimage" />
+      <h1 className="title">{card.title}</h1>
+      <p className="">{card.explanation}</p>
       <div className="stacks">
-        <ul>
+        <ul className="stack">
           <li>{card.stacks[0]}</li>
           <li>{card.stacks[1]}</li>
           <li>{card.stacks[2]}</li>
+          <li>{card.stacks[2]}</li>
+          <li>{card.stacks[3]}</li>
+          <li>{card.stacks[4]}</li>
+          <li>{card.stacks[5]}</li>
         </ul>
       </div>
       <div className="btn">
-        <button type="submit">Check source</button>
-        <button type="submit">Live demo</button>
+        <button type="submit"><a href={card.link[0]}>Check source</a></button>
+        <button type="submit"><a href={card.link[1]}>Live demo</a></button>
       </div>
     </div>
   </div>
@@ -27,6 +31,7 @@ Popup.propTypes = {
     image: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     explanation: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired,
     stacks: PropTypes.arrayOf(PropTypes.string).isRequired,
   }).isRequired,
 };
