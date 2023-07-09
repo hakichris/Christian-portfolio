a90a0fa10831a409bee520219944720da28ba65b
Chris <hakizachrist1@gmail.com>
2023-07-10 00:23:07 +0200

    Update popup.js

--- src/Actions/popup.js
+++ src/Actions/popup.js
@@ -1,30 +1,46 @@
 import { PropTypes } from 'prop-types';
 import '../CSS/card.css';
+import { SlClose } from 'react-icons/sl';
+import React, { useState } from 'react';
 
-const Popup = ({ card }) => (
-  <div className="popup-container">
-    <div className="popup-content">
-      <img className="popupimage" src={card.image} alt="cardimage" />
-      <h1 className="title">{card.title}</h1>
-      <p className="">{card.explanation}</p>
-      <div className="stacks">
-        <ul className="stack">
-          <li>{card.stacks[0]}</li>
-          <li>{card.stacks[1]}</li>
-          <li>{card.stacks[2]}</li>
-          <li>{card.stacks[2]}</li>
-          <li>{card.stacks[3]}</li>
-          <li>{card.stacks[4]}</li>
-          <li>{card.stacks[5]}</li>
-        </ul>
-      </div>
-      <div className="btn">
-        <button type="submit"><a href={card.link[0]}>Check source</a></button>
-        <button type="submit"><a href={card.link[1]}>Live demo</a></button>
-      </div>
-    </div>
-  </div>
-);
+const Popup = ({ card }) => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const closePopup = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <>
+      {isOpen && (
+        <div className="popup-container">
+          <div className="popupbtn">
+            <div className="closebtn" onClick={closePopup} onKeyDown={closePopup} aria-hidden>
+              <SlClose size={30} />
+            </div>
+            <div className="popup-content">
+              <img className="popupimage" src={card.image} alt="cardimage" />
+              <h1 className="title">{card.title}</h1>
+              <p className="">{card.explanation}</p>
+              <ul className="stack">
+                <li>{card.stacks[0]}</li>
+                <li>{card.stacks[1]}</li>
+                <li>{card.stacks[2]}</li>
+                <li>{card.stacks[3]}</li>
+                <li>{card.stacks[4]}</li>
+                <li>{card.stacks[5]}</li>
+              </ul>
+              <div className="btn">
+                <button type="submit"><a href={card.link[0]}>Check source</a></button>
+                <button type="submit"><a href={card.link[1]}>Live demo</a></button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
 
 Popup.propTypes = {
   card: PropTypes.shape({
