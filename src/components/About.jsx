998d15e0e378bf5be481a1134d73cccb3edca9c4
Chris <hakizachrist1@gmail.com>
2023-07-13 19:07:23 +0200

    Update about.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,18 +1,34 @@
 import '../CSS/aboutme.css';
 
-const About = () => (
-  <div className="about">
-    <div className="aboutmevideo">
-      <h1>About me</h1>
-      <iframe
-        className="youtubevideo"
-        src="https://www.youtube.com/embed/7WU__JQuB8g"
-        title="YouTube video player"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      />
+const About = () => {
+  const handleDownload = () => {
+    const resumeUrl = 'https://github.com/hakichris/Christian-portfolio/files/12041819/Christian.resume.software.engineer.pdf'; // Replace with your resume file URL
+    const link = document.createElement('a');
+    link.href = resumeUrl;
+    link.download = 'Christian hakizimana resume.pdf'; // Replace with the desired filename for the downloaded file
+    link.click();
+  };
+
+  return (
+
+    <div className="about">
+      <div className="aboutmevideo">
+        <h1>About me</h1>
+        <iframe
+          className="youtubevideo"
+          src="https://www.youtube.com/embed/7WU__JQuB8g"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      </div>
+      <div className="download-resume">
+        <button type="submit" onClick={handleDownload}>
+          Download my Resume
+        </button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default About;
