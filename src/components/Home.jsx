66169f19344300a132b9ddef9a7c327ee87e73ab
christian <hakizachrist1@outlook.com>
2025-02-20 01:01:05 +0200

    Rectified description

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -66,7 +66,7 @@ function Profile() {
             <span className="mentoring bold">
               {' '}
               React, Redux, JavaScript, Ruby on Rails, C++, Java, Leadership, Interpersonal skills,
-              Remote pair-programming, and cybersecurity,fluent in english and french. 
+              Remote pair-programming, cybersecurity, and fluent in english and french. 
             </span>
             {' '}
           </p>
